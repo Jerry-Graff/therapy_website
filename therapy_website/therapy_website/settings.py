5668b2c3446ca91c5ca8437de0aefa9b59efea9b
Kieran Moloney <k.moloney@hotmail.co.uk>
2025-04-10 10:02:53 +0100

    Deployment version 1.0.0

--- therapy_website/therapy_website/settings.py
+++ therapy_website/therapy_website/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joerobinsontherapy.com', 'www.joerobinsontherapy.com']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
